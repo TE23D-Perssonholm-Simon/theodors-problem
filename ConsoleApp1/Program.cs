269959df726ae7108f49dc8e71295d0275138112
Simon Perssonholm <Simon.perssonholm@elev.ga.ntig.se>
2024-10-22 17:20:42 +0200

    largest rect in histogram

--- ConsoleApp1/Program.cs
+++ ConsoleApp1/Program.cs
@@ -1,9 +1,38 @@
-﻿List<int>[] columns = new List<int>[7];
+﻿public class Solution {
+    public class Duo{
+        public int value = 0;
+        public int index = 0;
+        public Duo(int v, int i){
+            value = v;
+            index = i;
+        }
+    }
+    public int LargestRectangleArea(int[] heights) {
+        //stack men behöver ett sätt att lagra värdet och start index
+        Stack<Duo> searcher = new Stack<Duo>();
+        searcher.Push(new Duo(heights[0],0));
+        int maxArea = 0;
+        int index = 0;
+        for(int i = 1; i<heights.Count();i++){
+            index = i;
+            while(searcher.Peek().value > heights[i]){
 
-bool FourInARowCheck(int xpos)
-{
+                maxArea = int.Max(maxArea,searcher.Peek().value * (i-searcher.Peek().index));
+                index = searcher.Peek().index;
+                searcher.Pop();
+                if(searcher.Count == 0){
+                    break;
+                }
 
-}
-bool DirectionCheck(int xpos, int xchange, int ychange){
-    
+
+            }
+            searcher.Push(new Duo(heights[i],index));
+            
+            
+        }
+        foreach(Duo x in searcher){
+            maxArea = int.Max(maxArea,x.value * (heights.Count()-x.index));
+        }
+        return maxArea;
+    }
 }
